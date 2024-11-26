4cb64107cc65532a17715c5b87d78f6a8ec7ae67
rani-agrawal123 <agrawalrani2003@gmail.com>
2024-11-27 01:57:52 +0530

    loop

--- loop.js
+++ loop.js
@@ -1,1 +1,27 @@
-console.log("hello");
+// let counter = 1;
+// while(counter<=5){
+//     console.log("counter is:"+counter);
+//     counter++;
+// }
+// let n = 10;
+// while(n>=0){
+//     console.log(n);
+//     n--;
+// }
+
+// let x= 0;
+// while(x>=0){
+//     console.log("hello")
+//     x++;
+// }
+
+// let num = [10,20,30,40];
+// let index = 0;
+// let found = false;
+// while(index<num.length&&!found){
+//     if(num[index]===30){
+//         console.log("num 30 found at index"+index);
+//         found = true;
+//     }
+//     index++;
+// }
